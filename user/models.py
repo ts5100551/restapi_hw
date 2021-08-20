fcb2502c2948e349be283baa466879ad5564ffdf
ts5100551 <ts5100551@gmail.com>
2021-08-20 21:41:37 +0800

    Update model.py

--- user/models.py
+++ user/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     id = models.AutoField(auto_created=True, primary_key=True)
-    username = models.CharField(max_length=50, default='')
-    password = models.CharField(max_length=50, default='')
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
     email = models.EmailField(unique=True)
